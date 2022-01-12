e315a7853fe021de188ccdd6134d3b70ea5778f4
Stacey Nkongchu <91387650+Lunar-Trig@users.noreply.github.com>
2022-01-11 20:46:53 -0600

    Last Change

--- javascript/main.js
+++ javascript/main.js
@@ -2,25 +2,44 @@ const newTaskInput = document.querySelector('#task-input');
 const list = document.querySelector('.collection');
 const form = document.querySelector('.task-form');
 const clearTaskBtn = document.querySelector('.clear-btn');
-
+const saveBtn = document.querySelector('.btn-save-tasks')
 
 form.addEventListener('submit', addTask);
 clearTaskBtn.addEventListener('click', clearTasks);
+saveBtn.addEventListener('click', save)
 
 function addTask(e){
     if(newTaskInput.value  == ''  ){
         alert('Add New Task');
     }else{
-        list.innerHTML += ` 
-        <li> ${newTaskInput.value} </li>
-        `;
+        var deleteBtn = document.createElement('a');
+        var listItem = document.createElement('li'); 
+
+        listItem.innerText = newTaskInput.value;
+        listItem.className = 'collection-item';
+
+        deleteBtn.className = 'delete-item';
+        deleteBtn.innerHTML = '<i class="fas fa-times"></li>';
+        deleteBtn.addEventListener('click', removeTask);
+
+        listItem.appendChild(deleteBtn);
+        list.appendChild(listItem);
+        
+        function removeTask(){
+            if(confirm('Are You Sure?')){
+                listItem.remove();
+            };
+        }
     }
        
-        newTaskInput.value  == '' 
+        newTaskInput.value = '';
         e.preventDefault();
 };
 
 function clearTasks(){
     list.innerHTML = '';
 }
- 
+
+function save(){
+    localStorage.setItem('taskValue', newTaskInput.value);
+}
